cf0c76077e0bdbde9d9fdc07f97fdb949fdbc6c5
Gunjan Datta <me@dattabase.com>
2020-07-23 19:04:55 -0400

    Updated the base component reduce the # of unnecessary re-renders do to property updates.

---
+++ src/components/base.ts
@@ -0,0 +1,47 @@
+/**
+ * Base Template
+ */
+export default {
+    name: "Base",
+    inheritAttrs: false,
+    props: {
+        assignTo: { type: Function },
+        className: { type: String },
+    },
+    mounted() {
+        let updateFl = false;
+
+        // Parse the props
+        for (let prop in this.$props) {
+            // Add a watch for this property
+            this.$watch(prop, () => {
+                // See if an update is not taking place
+                if (!updateFl) {
+                    // Set the flag
+                    updateFl = true;
+
+                    // Wait for any other changes to occur before updating the element
+                    setTimeout(() => {
+                        // Clear the element
+                        while (this.$el.firstChild) {
+                            // Remove the child
+                            this.$el.removeChild(this.$el.firstChild);
+                        }
+
+                        // Set the flag
+                        updateFl = false;
+
+                        // Re-render the component
+                        this.$data.bs(this.$props);
+                    }, 100);
+                }
+            });
+        }
+
+        // Set the element
+        this.$props.el = this.$el;
+
+        // Render the component
+        this.$data.bs(this.$props);
+    },
+};
--- src/components/base.vue
+++ src/components/base.vue
@@ -1,31 +1,3 @@
 <template></template>
 
-<script>
-import Vue from "vue";
-export default {
-  name: "Base",
-  inheritAttrs: false,
-  props: {
-    assignTo: { type: Function },
-    className: { type: String }
-  },
-  mounted() {
-    // Parse the props
-    for (let prop in this.$props) {
-      // Add a watch for this property
-      this.$watch(prop, () => {
-        // Clear the element
-        while (this.$el.firstChild) {
-          this.$el.removeChild(this.$el.firstChild);
-        }
-        // Re-render the component
-        this.$data.bs(this.$props);
-      });
-    }
-    // Set the element
-    this.$props.el = this.$el;
-    // Render the component
-    this.$data.bs(this.$props);
-  }
-};
-</script>
+<script src="./base.ts"></script>
