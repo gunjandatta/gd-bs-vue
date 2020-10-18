e2de1a5276091d125c887a9bb4a4df461f318a92
Gunjan Datta <me@dattabase.com>
2020-10-18 16:35:12 -0400

    Fixed default export for base class.

--- build/components/base.d.ts
+++ build/components/base.d.ts
@@ -5,20 +5,3 @@ export declare class Base<T = any> extends Vue {
     protected _bs: any;
     mounted(): void;
 }
-declare const _default: {
-    name: string;
-    inheritAttrs: boolean;
-    props: {
-        assignTo: {
-            type: FunctionConstructor;
-        };
-        className: {
-            type: StringConstructor;
-        };
-    };
-    mounted(): void;
-};
-/**
- * Base Template
- */
-export default _default;
